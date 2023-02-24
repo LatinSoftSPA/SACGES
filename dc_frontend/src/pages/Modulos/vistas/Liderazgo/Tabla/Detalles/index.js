b480916f0ca5c729e8100a52e4e87618440068a0
mveraalbarenque <don.marcos.vera@gmail.com>
2023-02-23 23:18:33 -0300

    RF MOD-LID Tabla Detalles

--- dc_frontend/src/pages/Modulos/vistas/Liderazgo/Tabla/Detalles/index.js
+++ dc_frontend/src/pages/Modulos/vistas/Liderazgo/Tabla/Detalles/index.js
@@ -2,37 +2,29 @@ import { Flex, Table, TableBody, TableCell, TableRow } from "@tremor/react";
 
 import BuscarArchivos from "../../../../../../components/Botones/BuscarArchivos";
 
-const MediosRow = ({ medios, handleSeleccion, handleUpLoad }) => {
-  return medios.map((obj, i) => {
-    const { titulo, cargado } = obj;
-    return (
-      <TableRow key={i}>
-        <TableCell> {i + 1} </TableCell>
-        <TableCell> {titulo} </TableCell>
-        <TableCell>
-          <Flex justifyContent="justify-end">
-            <BuscarArchivos
-              cargado={cargado}
-              handleSeleccion={handleSeleccion}
-              handleUpLoad={handleUpLoad}
-            />
-          </Flex>
-        </TableCell>
-      </TableRow>
-    );
-  });
-};
-
 const TableBase = ({ medios, handleSeleccion, handleUpLoad }) => {
   return (
     <>
       <Table marginTop="mt-5">
         <TableBody>
-          <MediosRow
-            medios={medios}
-            handleSeleccion={handleSeleccion}
-            handleUpLoad={handleUpLoad}
-          />
+          {medios.map((obj, i) => {
+            const { titulo, cargado } = obj;
+            return (
+              <TableRow key={i}>
+                <TableCell> {i + 1} </TableCell>
+                <TableCell> {titulo} </TableCell>
+                <TableCell>
+                  <Flex justifyContent="justify-end">
+                    <BuscarArchivos
+                      cargado={cargado}
+                      handleSeleccion={handleSeleccion}
+                      handleUpLoad={handleUpLoad}
+                    />
+                  </Flex>
+                </TableCell>
+              </TableRow>
+            );
+          })}
         </TableBody>
       </Table>
     </>
